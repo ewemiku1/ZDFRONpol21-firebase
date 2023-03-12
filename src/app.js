3dc4464b951ea9c405b0051de9fa2093e5ea255c
ewemiku1 <116760550+ewemiku1@users.noreply.github.com>
2023-03-12 14:43:58 +0100

    update

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import './../styles/styles.css';
 // console.log("SIEMKA Z WEBPACK");
 
 import { initializeApp } from "firebase/app";
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { getDownloadURL, getStorage, listAll, ref, uploadBytes } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB6V6rhwqpyTBaLyGi1zwwpD0QifWShCB4",
@@ -79,13 +79,80 @@ setTimeout(() => {
 //   console.log(users.data);
 // }
 
+//1. Dodajemy input do HTMLu - typ file
+//2. Dodajemy przycisk do HTML 
+//3. Do przycisku obsluga klikniecia 
+//4. Jako callback wywolujemy linijki z prezentacji
 
-document.getElementById("myBtn").addEventListener("click", () => {
-  const file = document.getElementById("myFile").files[0];
-  const imageRef = ref(storage, "imageNew.jpg");
+// DODANIE MOŻLIWOSCI ZMIANY NAZWY PLIKU PRZEZ UŻYTKOWNIKA
+// 1. dodać input do HTML
+// 2. pobrac wpisana nazwe z imputu
+// 3. przekazac jako argument do funkcji
+// 4. callback do domyslnej nazwy pliku
 
-  uploadBytes(imageRef, file).then((uploadResult) => {
-    console.log("Sukces!");
-  })
-})
+// const headerInfo = document.getElementById("myHeader");
+// const fileNameInput = document.getElementById("myFileName");
 
+// document.getElementById("myBtn").addEventListener("click", () => {
+//   headerInfo.innerText = "przesyłam zdjęcie...";
+  
+//   const file = document.getElementById("myFile").files[0];
+//   let fileName = file.name;
+
+//   if (fileNameInput.value) {
+//     fileName = fileNameInput.value;
+//   }
+
+//   const imageRef = ref(storage, fileName); //wtedy tu drugą zmienną zamiast file.name, zmieniamy na naszą ustawioną FileName
+
+//   uploadBytes(imageRef, file).then((uploadResult) => {
+//     headerInfo.innerText = "Zdjęcie przesłano!";
+      
+//     getDownloadURL(imageRef).then(url => {
+//         const img = document.getElementById("myPhoto"); 
+//         img.src = url;
+//         img.style.width = "250px"
+//     })
+//   })
+// })
+
+//POBIERAMY WYBRANY PLIK
+// const imageRef = ref(storage, "imageNew.jpg");
+// getDownloadURL(imageRef).then(url => {
+//   const img = document.createElement("img");
+//   img.src = url;
+//   img.style.width = "250px"
+//   document.body.appendChild(img);
+// })
+
+// ZADANIE 4: UŻYTKOWNIK MOŻE WYBRAĆ KTÓRY PLIK POBRAĆ/ZOBACZYĆ
+//1. Dodac Input do podawania nazwy obrazka
+//2. Dodac przycisk do wyswietlania obrazka
+//3. Na klikniecie przycisku wyswietlic zdjecie
+//4. Przekazac nazwe do refa
+//5. Wyswietlic blad w headerInfo
+
+// const myShowFileNameInput = document.getElementById("myShowFileName");
+// const showFileBtn = document.getElementById("showPhotoBtn");
+// const img = document.createElement("img");
+
+// showFileBtn.addEventListener("click", () => {
+//   const imageRef = ref(storage, myShowFileNameInput.value);
+
+//   headerInfo.innerText = ""; // po to aby FOTO NIE ISTNIEJE, znikneło, gdy wybierzemy zdjęcie które istnieje
+
+  // getDownloadURL(imageRef).then(url => {
+  //   //const img = document.createElement("img"); --> przeniesienie (KILKA LINIJEK WYŻEJ) do 131, powoduje ze dodaje sie jedno zdjecie a nie kolejne i kolejne
+  //   img.src = url;
+  //   img.style.width = "250px"
+  //   document.body.appendChild(img);
+  // })
+//     .catch(ex => {   
+//       headerInfo.innerText = "FOTO NIE ISTNIEJE!!!";               
+//     })
+// });
+
+
+
+const storageRef = ref(storage);
+listAll(storageRef).then(res => console.log(res));
