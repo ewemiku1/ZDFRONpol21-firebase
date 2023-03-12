b0b18aa805edcdedd708a710c3fefd36b4e6d7e9
ewemiku1 <116760550+ewemiku1@users.noreply.github.com>
2023-03-12 10:38:15 +0100

    new new new

--- src/app.js
+++ src/app.js
@@ -25,3 +25,58 @@ const img = document.createElement("img");
 img.src = url;
 document.body.appendChild(img);
 
+function mojaFunkcja(callback) {
+  callback();
+}
+
+// funkcje strzałkowe, funkcje anonimowe, funkcje ... 
+// mojaFunkcja(() => console.log("MÓJ CALLBACK"));
+
+// PRZYKŁAD CALLBACKA
+/* 
+setTimeout(() => {
+  console.log("SIEMKA");
+}, 1000);
+*/
+
+document.body.addEventListener("click", () => {
+  console.log("CALLBACK");
+});
+
+// const mojaPupaJson = fetch().then((pupa123) => {
+//   return pupa123.json()
+// });
+// const mojaPupaJson = fetch().then((pupa123) => pupa123.json());
+
+// ~~linia 46-48 to to samo co 49, tylko inny zapis i obie działają z linią 52~~
+// mojaPupaJson.then((data) => console.log(data))
+
+// RÓŻNE MOŻLIWOŚCI ZAPISU TEGO SAMEGO:
+
+// const mojaPupaJson = fetch().then((pupa123) => pupa123.json());
+// mojaPupaJson.then((data) => console.log(data))
+
+// fetch()
+// .then((pupa123) => pupa123.json())
+// .then((data) => console.log(data));
+
+// async function mojAsynchronicznaFunkcja() {
+//   const pupa123 = await fetch();
+//   const data = await pupa123.json();
+//   console.log(data);
+// };
+
+// POBRANIE DANYCH UŻYTKOWNIKÓW Z LINKU PONIŻEJ, 
+// link do strony z notatkami na ten temat http://kursjs.pl/kurs/ajax/fetch
+fetch("https://reqres.in/api/users")
+.then((daneZPromisa) => daneZPromisa.json())
+.then((daneZJson) => console.log(daneZJson.data));
+
+// TO SAMO CO WYŻEJ,  INNY SPSB ZAPISU
+// async function myFunc () {
+//   const data = await fetch("https://reqres.in/api/users")
+//   const users = await data.json();
+//   console.log(users.data);
+// }
+
+
